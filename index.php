42b9d048f537bbc96ca4c1955d55c3b7c4839fe9
Iryna-polis <ira.polischuk.99@gmail.com>
2020-09-03 21:32:53 +0300

    ira03.09.2020 21:32

--- index.php
+++ index.php
@@ -5,9 +5,69 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ira-polis jfdkdjf</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/normalize.css" rel="stylesheet" type="text/css">
+    <title>Ira-polis</title>
 </head>
 <body>
-    <p>Lublu</p>
+
+<header class="header">
+    <div class="menu">
+        <div class="container">
+            <nav class="menu__nav">
+                <a href="#" class="menu__link">Главная</a>
+                <a href="#" class="menu__link">Сайты</a>
+                <a href="#" class="menu__link">Каталог</a>
+                <a href="#" class="menu__link">Примеры работ</a>
+                <a href="#" class="menu__link">Шаблоны</a>
+                <a href="#" class="menu__link">Стоимость</a>
+                <a href="#" class="menu__link">О нас</a>
+            </nav>
+        </div>
+        <!-- /.container-->
+    </div>
+        <!-- /.menu -->
+    <div class="navbar">
+        <div class="container">
+            <div class="navbar-wrap">
+                <button class="menu-button navbar__menu-button">
+                    <span class="menu-button__line"></span>
+                    <span class="menu-button__line"></span>
+                    <span class="menu-button__line"></span>
+                </button>
+                <div class="logo navbar__logo">
+                    <strong class="logo_text">Polishchuk</strong>
+                    <span class="logo_description">разработка сайтов</span>
+                </div>
+                <div class="navbar_mobile">
+                    <a class="navbar_phone" href="tel:+380730406829">+38(073)-040-68-29</a>
+                    <span class="novbar_work-time navbar__small-text">без выходных 9-21</span>
+                </div>
+                <div class="navbar_landline">
+                    <a class="navbar_phone" href="tel:+380730406829">+38(073)-040-68-29</a>
+                    <a class="novbar_email navbar__small-text" href="mailto:ira.polischuk.99@gmail.com">ira.polischuk.99@gmail.com</a>
+                </div>
+                <button class="button-o navbar_button">Получить консультацию</button>
+                <!-- /.logo navbar__logo --></div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.navbar -->
+    <div class="hero">
+        <div class="container">
+            <div class="backgraund">
+                <h1 class="hero__title">
+                    Гарантия <br>максимального качества <br> за краткий период
+                </h1>
+                <span class="hero__subtitle">от Polishchuk</span>
+                <button class="button hero_button">обратная связь</button>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.hero -->
+</header>
+<!-- /.header -->
+
 </body>
 </html>
